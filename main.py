0a826dea25832abad3d1380b23f5e4267d5f9cd2
Rares Chelariu <Rares.Chelariu@nuvei.com>
2023-03-22 20:43:24 +0200

    save tweet list as csv

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 import datetime
 
+import pandas as pd
 import seaborn as sns
 import snscrape.modules.twitter as sntwitter
-from tqdm.notebook import tqdm_notebook
 
 sns.set_theme(style="whitegrid")
 
-text = input('Enter query text to be matched (or leave it blank by pressing enter)')
-username = input('Enter specific username(s) from a twitter account without @ (or leave it blank by pressing enter): ')
-since = input('Enter startdate in this format yyyy-mm-dd (or leave it blank by pressing enter): ')
-until = input('Enter enddate in this format yyyy-mm-dd (or leave it blank by pressing enter): ')
-retweet = input('Exclude Retweets? (y/n): ')
-replies = input('Exclude Replies? (y/n): ')
+initialQuery = input('Enter query text to be matched (or leave it blank by pressing enter)')
+twitterUser = input('Enter specific username(s) from a twitter account (or leave it blank by pressing enter): ')
+startDate = input('Enter startdate in this format yyyy-mm-dd (or leave it blank by pressing enter): ')
+endDate = input('Enter enddate in this format yyyy-mm-dd (or leave it blank by pressing enter): ')
+excludeRetweets = input('Exclude Retweets? (y/n): ')
+excludeReplies = input('Exclude Replies? (y/n): ')
 
 
 def search(text, username, since, until, retweet, replies):
-    global filename
     q = text
     if username != '':
         q += f" from:{username}"
@@ -30,25 +29,24 @@ def search(text, username, since, until, retweet, replies):
         q += f" exclude:retweets"
     if replies == 'y':
         q += f" exclude:replies"
-    if username != '' and text != '':
-        filename = f"{since}_{until}_{username}_{text}.csv"
-    elif username != "":
-        filename = f"{since}_{until}_{username}.csv"
-    else:
-        filename = f"{since}_{until}_{text}.csv"
-    print(filename)
     return q
 
 
-q = search(text, username, since, until, retweet, replies)
-# Creating list to append tweet data
-tweets_list1 = []
+def get_tweets_as_file(text, username, since, until, retweet, replies):
+    q = search(text, username, since, until, retweet, replies)
+    # Creating list to append tweet data
+    tweets_list1 = []
 
-# Using TwitterSearchScraper to scrape data and append tweets to list
-for i, tweet in enumerate(sntwitter.TwitterSearchScraper(q).get_items()):
-    tweets_list1.append([tweet.date, tweet.id, tweet.rawContent, tweet.user.username, tweet.lang,
-                         tweet.hashtags, tweet.replyCount, tweet.retweetCount, tweet.likeCount, tweet.quoteCount,
-                         tweet.media])
+    # Using TwitterSearchScraper to scrape data and append tweets to list
+    for i, tweet in enumerate(sntwitter.TwitterSearchScraper(q).get_items()):
+        tweets_list1.append([tweet.date, tweet.id, tweet.rawContent, tweet.user.twitterUser, tweet.lang,
+                             tweet.hashtags, tweet.replyCount, tweet.retweetCount, tweet.likeCount, tweet.quoteCount,
+                             tweet.media])
 
-print(tweets_list1)
+    tweets_df1 = pd.DataFrame(tweets_list1, columns=['DateTime', 'TweetId', 'Text', 'Username', 'Language',
+                                                     'Hashtags', 'ReplyCount', 'RetweetCount', 'LikeCount',
+                                                     'QuoteCount', 'Media'])
+    tweets_df1.to_csv(f"{twitterUser}", index=False)
 
+
+get_tweets_as_file(initialQuery, twitterUser, startDate, endDate, excludeRetweets, excludeReplies)
